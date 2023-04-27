0178e324ab8255a312326f78aa3ca24c6839d556
Gueka <marcojcapo@gmail.com>
2023-04-26 20:41:38 -0400

    Added pause mechanism

--- src/main.js
+++ src/main.js
@@ -1,47 +1,59 @@
 // Import the program runner
-import {run} from './run.js'
+import { run } from './run.js';
 import '../css/style.css';
 
-let controls = document.querySelector('.controlsHolder')
-controls.addEventListener('mouseover', () => {controls.style.opacity = '1'})
-controls.addEventListener('mouseout', () => {controls.style.opacity = '.25'})
+let controls = document.querySelector('.controlsHolder');
+controls.addEventListener('mouseover', () => {
+  controls.style.opacity = '1';
+});
+controls.addEventListener('mouseout', () => {
+  controls.style.opacity = '.25';
+});
 
 // Import a custom program; with at least a main() function exported
 // import * as program from './wireframe_cube.js'
-import * as program from './programs/demos/doom_flame_customized.js'
+import * as program from './programs/demos/doom_flame_customized.js';
 // const p = window.parent;
 
 let fpsValue = 30;
 
-let buttonFPS12 = document.querySelector('#buttonFPS12')
-let buttonFPS24 = document.querySelector('#buttonFPS24')
-let buttonFPS30 = document.querySelector('#buttonFPS30')
+let buttonFPS12 = document.querySelector('#buttonFPS12');
+let buttonFPS24 = document.querySelector('#buttonFPS24');
+let buttonFPS30 = document.querySelector('#buttonFPS30');
 
 // Run settings can override the default- and program seetings.
 // See the API for details.
 const settings = {
   element: document.querySelector('#ASCII-Holder'),
-  backgroundColor : 'transparent', 
-  color : '#ff4800',
-  fps: fpsValue
-}
+  backgroundColor: 'transparent',
+  color: '#ff4800',
+  fps: fpsValue,
+};
 
 buttonFPS12.onclick = () => {
-  window.location.replace("https://pablocarreira.github.io/hiro-so-ASCII_Animations/FPS12/");
+  window.location.replace(
+    'https://pablocarreira.github.io/hiro-so-ASCII_Animations/FPS12/'
+  );
 };
 
 buttonFPS24.onclick = () => {
-  window.location.replace("https://pablocarreira.github.io/hiro-so-ASCII_Animations/FPS24/");
+  window.location.replace(
+    'https://pablocarreira.github.io/hiro-so-ASCII_Animations/FPS24/'
+  );
 };
 
-
 buttonFPS30.onclick = () => {
-  window.location.replace("https://pablocarreira.github.io/hiro-so-ASCII_Animations/FPS30/");
+  window.location.replace(
+    'https://pablocarreira.github.io/hiro-so-ASCII_Animations/FPS30/'
+  );
+};
+
+buttonPause.onclick = () => {
+  document.querySelector('#ASCII-Holder').dispatchEvent(new Event('pause'));
 };
 
 // Boot (returns a promise)
-run(program, settings).catch(function(e){
-  console.warn(e.message)
-  console.log(e.error)
-})
-
+run(program, settings).catch(function (e) {
+  console.warn(e.message);
+  console.log(e.error);
+});
